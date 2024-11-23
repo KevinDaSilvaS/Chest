931d3e6b4526b5e3a1b2dcea60736ddba8ef5756
Kevin <kevin.kds80@gmail.com>
2024-11-23 18:54:23 -0300

    add first commands to CLI

---
+++ src/main.v
@@ -0,0 +1,34 @@
+module main
+
+import os
+import cli
+import commands as c
+
+fn main() {
+    mut app := cli.Command{
+        name:        'Chest'
+        description: 'Chest Password Manager'
+        execute:     fn (cmd cli.Command) ! {
+            c.help()
+            return
+        }
+        commands:    [
+            cli.Command{
+                name:    'init'
+                execute: fn (cmd cli.Command) ! {
+                    if os.args.len >= 3 {
+						token := os.args[1]
+						password := os.args[2]
+						c.init(token, password)
+						return
+					}
+					c.help()
+                    return 
+                }
+            },
+        ]
+    }
+    app.setup()
+    app.parse(os.args)
+	println(os.args)
+}
---
+++ v.mod
@@ -0,0 +1,7 @@
+Module {
+	name: 'chest'
+	description: 'password manager'
+	version: '0.0.0'
+	license: 'MIT'
+	dependencies: []
+}
